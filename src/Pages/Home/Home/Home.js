7460f21e63a4eb193f82008f97946f5c3c671007
atiq100 <atiqur1997rj@gmail.com>
2022-11-08 04:14:37 +0600

    stat added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Hero from '../Hero/Hero';
+import Statistic from '../Statistic/Statistic';
 
 const Home = () => {
     return (
         <div>
             <Hero></Hero>
             <Banner></Banner>
+            <Statistic></Statistic>
         </div>
     );
 };
