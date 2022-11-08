b4c2b7cbb6c11f56a1abc99a8e20f608f62b9b92
atiq100 <atiqur1997rj@gmail.com>
2022-11-08 16:56:57 +0600

    fix some responsive issues

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,19 +1,18 @@
-
 import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
-import logo from '../../../assets/logo.png'
+import logo from "../../../assets/logo.png";
 import { AuthContext } from "../../../context/AuthProvider/AuthProvider";
 
 export const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const {user,logout} = useContext(AuthContext)
+  const { user, logout } = useContext(AuthContext);
 
-  const handleLogout = ()=>{
+  const handleLogout = () => {
     logout()
-    .then( ()=> {})
-    .catch(error=>console.log(error))
-  }
+      .then(() => {})
+      .catch((error) => console.log(error));
+  };
 
   return (
     <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
@@ -71,34 +70,33 @@ export const Header = () => {
             </Link>
           </li>
           <li>
-           {
-            user?.uid ?
-            <Link
-            onClick={handleLogout}
-            to="/"
-            className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-sky-500/100 hover:bg-sky-700/100 focus:shadow-outline focus:outline-none"
-            aria-label="Sign out"
-            title="Sign out"
-          >
-            Sign out
-          </Link>
-          :
-          <Link
-          to="/login"
-          className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-sky-500/100 hover:bg-sky-700/100 focus:shadow-outline focus:outline-none"
-          aria-label="Sign in"
-          title="Sign in"
-        >
-          Sign in
-        </Link>
-           }
+            {user?.uid ? (
+              <Link
+                onClick={handleLogout}
+                to="/"
+                className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-sky-500/100 hover:bg-sky-700/100 focus:shadow-outline focus:outline-none"
+                aria-label="Sign out"
+                title="Sign out"
+              >
+                Sign out
+              </Link>
+            ) : (
+              <Link
+                to="/login"
+                className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-sky-500/100 hover:bg-sky-700/100 focus:shadow-outline focus:outline-none"
+                aria-label="Sign in"
+                title="Sign in"
+              >
+                Sign in
+              </Link>
+            )}
           </li>
         </ul>
         <div className="lg:hidden">
           <button
             aria-label="Open Menu"
             title="Open Menu"
-            className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
+            className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-purple-50 focus:bg-purple-50"
             onClick={() => setIsMenuOpen(true)}
           >
             <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
@@ -117,7 +115,7 @@ export const Header = () => {
             </svg>
           </button>
           {isMenuOpen && (
-            <div className="absolute top-0 left-0 w-full">
+            <div className="absolute top-0 left-0 w-full z-10">
               <div className="p-5 bg-white border rounded shadow-sm">
                 <div className="flex items-center justify-between mb-4">
                   <div>
@@ -192,14 +190,26 @@ export const Header = () => {
                       </Link>
                     </li>
                     <li>
-                      <Link
-                        to="/login"
-                        className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-sky-500/100 hover:bg-sky-700/100 focus:shadow-outline focus:outline-none"
-                        aria-label="Sign in"
-                        title="Sign in"
-                      >
-                        Sign in
-                      </Link>
+                      {user?.uid ? (
+                        <Link
+                          onClick={handleLogout}
+                          to="/"
+                          className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-sky-500/100 hover:bg-sky-700/100 focus:shadow-outline focus:outline-none"
+                          aria-label="Sign out"
+                          title="Sign out"
+                        >
+                          Sign out
+                        </Link>
+                      ) : (
+                        <Link
+                          to="/login"
+                          className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-sky-500/100 hover:bg-sky-700/100 focus:shadow-outline focus:outline-none"
+                          aria-label="Sign in"
+                          title="Sign in"
+                        >
+                          Sign in
+                        </Link>
+                      )}
                     </li>
                   </ul>
                 </nav>
