686cf6a03fff08429236258f0d8b3faa35d3dad7
atiq100 <atiqur1997rj@gmail.com>
2022-11-08 16:38:02 +0600

    add login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,8 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { GoogleAuthProvider } from 'firebase/auth';
+import React, { useContext, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import image from '../../assets/login/welcome.png'
+import { AuthContext } from '../../context/AuthProvider/AuthProvider';
 
 const Login = () => {
+    const [error,setError] = useState('')
+    const googleProvider = new GoogleAuthProvider()
+    const {signIn,providerLogin} = useContext(AuthContext);
+    const navigate = useNavigate();
+    const location = useLocation()
+
+    const from = location.state?.from?.pathname || '/';
+
+    const handleSubmit = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        signIn(email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+            form.reset();
+            setError('')
+           
+            navigate(from,{replace: true}) 
+            
+        })
+        .catch(error =>{
+             console.error(error)
+             setError(error.message)
+            })
+    }
+    const handleGoogleSignIn=()=>{
+        providerLogin(googleProvider)
+        .then(result =>{
+            const user = result.user
+           
+           navigate(from,{replace: true})
+            
+           
+        })
+        .catch(error => console.log(error))
+
+    }
     return (
       
        <div className='flex lg:flex-row flex-col mx-16 items-center gap-4'>
@@ -15,7 +57,7 @@ const Login = () => {
 		<Link to="/signup" rel="noopener noreferrer" className="focus:underline hover:underline">Sign up here</Link>
 	</p>
 	<div className="my-6 space-y-4">
-		<button aria-label="Login with Google" type="button" className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-sky-400">
+		<button onClick={ handleGoogleSignIn} aria-label="Login with Google" type="button" className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-sky-400">
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
 				<path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
 			</svg>
@@ -29,7 +71,7 @@ const Login = () => {
 		<p className="px-3 dark:text-gray-400">OR</p>
 		<hr className="w-full dark:text-gray-400"/>
 	</div>
-	<form  className="space-y-8 ">
+	<form onSubmit={handleSubmit} className="space-y-8 ">
 		<div className="space-y-4">
 			<div className="space-y-2">
 				<label htmlFor="email" className="block text-sm">Email address</label>
@@ -43,7 +85,8 @@ const Login = () => {
 				<input type="password" name="password"  placeholder="*****" className="w-full px-3 py-2 border rounded-md dark:border-gray-700  dark:text-gray-400 focus:dark:border-sky-400" />
 			</div>
 		</div>
-		<button type="button" className="w-full px-8 py-3 font-semibold rounded-md bg-sky-500/100 dark:text-gray-900">Sign in</button>
+		<button type="submit" className="w-full px-8 py-3 font-semibold rounded-md bg-sky-500/100 dark:text-gray-900">Sign in</button>
+        <p>{error}</p>
 	</form>
 </div>
        </div>
