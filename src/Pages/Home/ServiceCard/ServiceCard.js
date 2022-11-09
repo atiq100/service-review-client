5c420d6ec62abb5c19ba79c0e24caed358eea097
atiq100 <atiqur1997rj@gmail.com>
2022-11-09 21:40:44 +0600

    add 100 char on home description

--- src/Pages/Home/ServiceCard/ServiceCard.js
+++ src/Pages/Home/ServiceCard/ServiceCard.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaArrowRight, FaBeer } from 'react-icons/fa';
 
 const ServiceCard = ({service}) => {
 	const {img, description, title,_id} = service;
     return (
-        <div className="max-w-xs rounded-md shadow-md dark:bg-gray-900 dark:text-gray-100">
+        <div className="max-w-xs rounded-md shadow-md  dark:text-gray-900">
 	<img src={img} alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
 	<div className="flex flex-col justify-between p-6 space-y-8">
 		<div className="space-y-2">
-			<h2 className="text-3xl font-semibold tracking-wide">{title}</h2>
-			<p className="dark:text-gray-100">{description}</p>
+			<h2 className="text-2xl font-semibold ">{title}</h2>
+			<p className="dark:text-gray-600">
+			{
+                        description.length > 100 ?
+                            <>{description.slice(0, 100) + '...'} <Link to={`/service/${_id}`}> <FaArrowRight className='ml-56 text-xl text-sky-500/100'></FaArrowRight> </Link> </>
+                            :
+                            description
+                    }
+			</p>
 		</div>
-		<button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-400 dark:text-gray-900">Read more</button>
+		
 	</div>
 </div>
     );
