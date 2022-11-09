599eed7ca2b8a8ce98f090c872f181e492a7169e
atiq100 <atiqur1997rj@gmail.com>
2022-11-09 21:05:07 +0600

    services data added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Hero from '../Hero/Hero';
+import Services from '../Services/Services';
 import Statistic from '../Statistic/Statistic';
 
 const Home = () => {
     return (
         <div>
             <Hero></Hero>
+            <Services></Services>
             <Banner></Banner>
             <Statistic></Statistic>
         </div>
